0f17db7f58884a55f35562f30e60a81b99ae45cb
TaxionX <luiscarlosgarciavillalobos@gmail.com>
2024-08-19 20:32:20 +0000

    trabajo base

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let simbolos = ["♦", "♥", "♠", "♣"];
+  let colores = ["red", "black"];
+  let cartas = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Rey",
+    "Reina",
+    "Jota",
+    "As"
+  ];
+
+  let simbolosAleatorios = Math.floor(Math.random() * simbolos.length);
+  let coloresAleatorios = Math.floor(Math.random() * colores.length);
+  let cartasAleatorios = Math.floor(Math.random() * cartas.length);
+
+  let textocartas = document.getElementById("cartas");
+  textocartas.innerText = cartas[cartasAleatorios];
+
+  let elementos = document.querySelectorAll(".simbolos");
+
+  elementos.forEach(element => {
+    element.style.color = colores[coloresAleatorios];
+    element.innerText = simbolos[simbolosAleatorios];
+  });
 };
